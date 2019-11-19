e8e96aa482d565c30ee76ce5a1e9b947e9040bad
Jonathan <aguezak@gmail.com>
2019-11-18 19:44:19 -0500

    Question 3 et 4 de la première partie avec les images nécéssaires

--- projet/dog.py
+++ projet/dog.py
@@ -24,17 +24,21 @@ def DoG(img, s, nb_octave):
     for octave in range(nb_octave):
         for scale in range(s):
             imgcpy = applyGaussian(img)
-            results.append(abs(img - imgcpy))
+            results.append(abs(img - imgcpy))  #original line to get difference of Gaussian
+            #results.append(imgcpy)              #test line to get filtered images
             img = imgcpy
         img = resize(img)
     return results
 
 
 if __name__ == '__main__':
-    img = openImage('droite.jpg')
+    #img = openImage('droite.jpg')
+    img = openImage('Lenna.jpg')
+    
     img = img / np.max(img)
 
-    results = DoG(img, 3, 2)
+    #results = DoG(img, 3, 2)
+    results = DoG(img, 6, 1)
     plt.plot([2,3])
     plt.subplot(231)
     show(results[0])
@@ -46,6 +50,15 @@ if __name__ == '__main__':
     show(results[3])
     plt.subplot(235)
     show(results[4])
-    plt.subplot(236)
-    show(results[5])
+    #plt.subplot(236)
+    #show(results[5])
     plt.show()
+
+    #plt.plot([1,3])
+    #plt.subplot(131)
+    #show(results[0])
+    #plt.subplot(132)
+    #show(results[1])
+    #plt.subplot(133)
+    #show(results[2])
+    #plt.show()
