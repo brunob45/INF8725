30226d722eab41eac2a7c79600c288b3ac6781e3
Jonathan <aguezak@gmail.com>
2019-12-04 23:01:12 -0500

    trouver les 10 points les plus proches

--- projet/homographie.py
+++ projet/homographie.py
@@ -11,12 +11,22 @@ def distanceInterPoints(keypoints1, keypoints2):
 
     dists = np.empty((keypoints1.__len__(),keypoints2.__len__()), dtype=float)
     print("Range : ", keypoints1.__len__()*keypoints2.__len__())
+    #mins = []
+
     for k1 in range(0,keypoints1.__len__()):
         for k2 in range(0, keypoints2.__len__()):
             dists[k1][k2] = distanceEuclidienne(keypoints1[k1], keypoints2[k2])
-            print(dists[k1][k2], " : ", k1*k2, " out of ",keypoints1.__len__()*keypoints2.__len__())
+            #if k1*keypoints2.__len__()+k2==112895:
+                #print("stop")
+            #print(dists[k1][k2], " : ", k1*keypoints2.__len__()+k2, " out of ",keypoints1.__len__()*keypoints2.__len__())
 
-    return dists
+            #if mins.__len__() < 10:
+                #mins.append(dists[k1][k2])
+            #else:
+                #if max(mins) > dists[k1][k2]:
+                    #mins[np.argmax(mins)] = dists[k1][k2]
+
+    return dists#,mins
 
 def distanceEuclidienne(p1,p2):
     dists = [];
@@ -51,6 +61,52 @@ if __name__ == '__main__':
     descDroite = getDescriptorsImage('droite.jpg')
     descGauche = getDescriptorsImage('gauche.jpg')
 
-    mat = distanceInterPoints(descDroite, descGauche)
+    #mat = distanceInterPoints(descDroite, descGauche)
+    #np.save("distances.npy", mat)
 
-    np.save("couples.npy", mat)
+    distMat = np.load("./distances.npy")
+
+    keypointsMin1 = []
+    keypointsMin2 = []
+
+    for i in range(0,10):
+        ks = np.unravel_index(distMat.argmin(), distMat.shape)
+        k1 = ks[0]
+        k2 = ks[1]
+        distMat[k1][k2] += 9999999
+        keypointsMin1.append(descDroite[k1])
+        keypointsMin2.append(descGauche[k2])
+
+    plt.subplot(122)
+    show(openImage('droite.jpg'))
+
+    x = []
+    y = []
+    for k1 in keypointsMin1:
+        x.append(k1[0])
+        y.append(k1[1])
+    plt.plot(x, y, marker='o', linewidth=0, markersize=5)
+
+    plt.subplot(121)
+    show(openImage('gauche.jpg'))
+
+    x = []
+    y = []
+    for k2 in keypointsMin2:
+        x.append(k2[0])
+        y.append(k2[1])
+    plt.plot(x, y, marker='o', linewidth=0, markersize=5)
+    plt.show() 
+        
+        #coordMin = np.empty
+
+    #ax = plt.subplot(111)
+    #show(img)
+    #x = []
+    #y = []
+    #for (i,j,s,a,l) in mat:
+        #x.append(mins[0])
+        #y.append(mins[0])
+
+    #ax.plot(x, y, marker='o', linewidth=0, markersize=5, label=str(o))
+    #plt.show()
